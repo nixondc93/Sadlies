eddf13c900f0bc14076dcfdb97649d38f0afe9db
Derry Nixon <nixondc93@gmail.com>
2017-02-20 17:41:49 -0800

    modified twitter.js

--- twitter.js
+++ twitter.js
@@ -2,9 +2,6 @@ const Twit = require('twit');
 const Fs = require('file-system');
 const Tweets = require('./trumpTweets');
 
-
-
-
 const T = new Twit({
   consumer_key: 'N1bCISbBQDYHfohkAfNBKqStY',
   consumer_secret: 'tYuH96sqwHzYa4hSpRKf7WlVZDhW5b1vxFK800Bxw1rk9o9ty7',
@@ -12,19 +9,20 @@ const T = new Twit({
 });
 
 
+
 let tweets = [],
   tweetCount = 0;
 
 const Interval = setInterval(() => {
 
-  let start = tweets.length === 0 ? 833050081641234400 : tweets[tweets.length - 1].id
+  let start = tweets.length === 0 ? 833050081641234400 : tweets[tweets.length - 1].id 
 
   T.get('statuses/user_timeline', {
     screen_name: 'realDonaldTrump',
     count: 200,
     max_id: start
   }, function (err, data, response) {
-
+    
     if (err) {
       console.log("Error fectching tweets");
     }
@@ -37,5 +35,5 @@ const Interval = setInterval(() => {
     tweets = tweets.concat(data);
     console.log("Number of tweets: ", tweetCount);
   });
-
+  
 }, 2000);
